45978885e42dd888c7357b9600d7ab1be814c1e5
notlemos <caiolemossfc123@hotmail.com>
2026-05-11 17:53:25 -0300

    Fundamentos de DDD - Classe base de entidades

--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,12 +1,12 @@
-import { randomUUID } from "node:crypto"
+import { Entity } from "../../core/entities/entity"
 
-export class Student {
-    public id: string
-    public name: string
+interface StudentProps {
+    name: string
+}
 
+export class Student extends Entity<StudentProps> {
+    constructor(props: StudentProps, id?: string) {
+        super(props, id)
 
-    constructor(name: string, id?: string) {
-        this.name = name
-        this.id = id ?? randomUUID()
     }
 }
