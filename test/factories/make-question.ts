87895c3a3c86e30340caa5210e9684e6ea9090d5
notlemos <caiolemossfc123@hotmail.com>
2026-05-14 17:36:32 -0300

    Fórum com DDD & Clean Architecture - Gerando dados fictícios

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,16 +1,23 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
+export function makeQuestion(
+    override: Partial<QuestionProps> = {},
+    id?: UniqueEntityId,
+) {
   const question = Question.create({
     authorId: new UniqueEntityId(),
-    title: 'Example',
-    content: 'Example Content',
+    title: faker.lorem.sentence(),
+    content: faker.lorem.text(),
     ...override,
-  })
+  },
+  id,
+ )
 
   return question
 }
