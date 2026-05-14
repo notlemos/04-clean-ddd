2cc87e14174e3dad6e4a5d396f03a6520044e0d0
notlemos <caiolemossfc123@hotmail.com>
2026-05-14 16:54:22 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Buscar pergunta pelo slug

---
+++ src/domain/forum/application/use-cases/get-quetion-by-slug.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { Question } from '../../enterprise/entities/question'
+import { Slug } from '../../enterprise/entities/values-objects/slug'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  // System Under Test = SUT
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = Question.create({
+      authorId: new UniqueEntityId(),
+      title: 'Example question',
+      slug: Slug.create('example-question'),
+      content: 'Example Content',
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toEqual(newQuestion.title)
+  })
+})
