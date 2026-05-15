7904b0d2f5218debe7e2da9bf3d2d45fc6999b51
notlemos <caiolemossfc123@hotmail.com>
2026-05-15 15:38:55 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Editar resposta

---
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -0,0 +1,40 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswerRepository } from '../repositories/answer-repository'
+
+interface EditAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+interface EditAnswerUseCaseResponse {
+  answer: Answer
+}
+
+export class EditAnswerUseCase {
+  constructor(private answersRepository: AnswerRepository) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer Not Found!')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('Not Allowed.')
+    }
+
+    answer.content = content
+
+    await this.answersRepository.save(answer)
+
+    return {
+      answer,
+    }
+  }
+}
--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -2,7 +2,6 @@ import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questio
 import { makeQuestion } from 'test/factories/make-question'
 import { EditQuestionUseCase } from './edit-question'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { title } from 'node:process'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: EditQuestionUseCase
