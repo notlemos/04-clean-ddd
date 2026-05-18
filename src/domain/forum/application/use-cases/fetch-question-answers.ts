7024c0c923afc84a935b7bc7c1f444fa265eb8ce
notlemos <caiolemossfc123@hotmail.com>
2026-05-18 15:37:31 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Listar respostas da pergunta

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,29 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswerRepository } from '../repositories/answer-repository'
+
+interface FetchQuestionsAnswersUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionsAnswersUseCaseResponse {
+  answers: Answer[]
+}
+
+export class FetchQuestionsAnswersUseCase {
+  constructor(private answersRepository: AnswerRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionsAnswersUseCaseRequest): Promise<FetchQuestionsAnswersUseCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page },
+    )
+
+    return {
+      answers,
+    }
+  }
+}
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerRepository } from '@/domain/forum/application/repositories/answer-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
@@ -12,6 +13,14 @@ export class InMemoryAnswersRepository implements AnswerRepository {
     }
 
     return answer
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async create(answer: Answer) {
