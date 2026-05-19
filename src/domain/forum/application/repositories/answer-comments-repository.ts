aca16c85511bcaf9e314824570c6488242f5f649
notlemos <caiolemossfc123@hotmail.com>
2026-05-19 15:40:08 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Listar comentários da resposta

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,7 +1,13 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswerCommentsRepository {
   findById(id: string): Promise<AnswerComment | null>
+  findManyByAnswerId(
+    answerId: string,
+    params: PaginationParams,
+  ): Promise<AnswerComment[]>
   create(answerComment: AnswerComment): Promise<void>
   delete(answerComment: AnswerComment): Promise<void>
 }
