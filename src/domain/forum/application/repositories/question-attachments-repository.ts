95cdea1ba45cea40512f120e9ca0f6c970e1713e
notlemos <caiolemossfc123@hotmail.com>
2026-05-22 15:30:48 -0300

    Aggregates & Watched Lists - Editando pergunta com anexos

---
+++ src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -0,0 +1,6 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
+
+export interface QuestionAttachmentsRepository {
+  findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]>
+}
